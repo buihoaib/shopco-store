14b4707c5faf4b507ed099bfb2535fe5103153d9
Trang Bui <buihoaib@gmail.com>
2023-09-30 18:57:28 +0700

    Styled NotFound component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function NotFound() {
     return (
-        <div className='flex flex-col bg-stone-100 text-black text-center justify-center flex-1'>
+        <div className='flex flex-col h-screen bg-stone-100 text-black text-center justify-center flex-1'>
             <p className='text-3xl font-bold uppercase'>Page not found</p>
             <p>We could not find the page you were looking for.</p>
             <p>Go back to the <Link href='/' className='hover:opacity-50 underline'>homepage</Link>.</p>
